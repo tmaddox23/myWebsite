fe35d011947058f3a961e50a955d0710a47b0911
tmaddox23 <tmaddox@uwo.ca>
2024-06-20 22:41:13 -0400

    I have added my footer component and updated my contact form

--- my-website-app/src/app/footer/footer.component.ts
+++ my-website-app/src/app/footer/footer.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
-export class AppComponent {
+export class FooterComponent {
 
   constructor(public dialog: MatDialog) {}
 
